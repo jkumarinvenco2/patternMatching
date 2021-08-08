6b09bb2ceaa39edfb8228bb58f2e7574df5f9cf3
JK <Jitender.Kumar@invenco.com>
2021-08-09 11:10:04 +1200

    pattern matching try

---
+++ patternMatching.py
@@ -0,0 +1,22 @@
+#trial pattern matching
+def isPhoneNumber(text):
+    if len(text) != 12:
+        return False
+    for i in range(0,3):
+        if not text[i].isdecimal():
+            return False
+    if text[3] != '-':
+        return False
+    for i in range(4,7):
+        if not text[i].isdecimal():
+            return False
+    if text[7] != '-':
+        return False
+    for i in range(8,12):
+        if not text[i].isdecimal():
+            return False
+
+    return True
+
+print('232-234-2344 is a phone number')
+print(isPhoneNumber('934-234-2344'))
